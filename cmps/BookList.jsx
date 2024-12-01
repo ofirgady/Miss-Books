2f7aee201624d05f4a474b811a55e81a56058c55
ofirgady <OFirgady@gmail.com>
2024-12-01 20:03:10 +0200

    deployment bug v1

--- cmps/BookList.jsx
+++ cmps/BookList.jsx
@@ -3,9 +3,10 @@ import { BookPreview } from "./BookPreview.jsx"
 const { useState, useEffect, useRef } = React
 
 export function BookList({ books, onRemoveBook }) {
+    console.log("list of books frrom BookList", books);
     return (
     <section className="book-list">
-        {books.map(book =>
+        {books && books.map(book =>
         <BookPreview key={book.id} book={book} onRemoveBook={onRemoveBook} />
         )}
     </section>
